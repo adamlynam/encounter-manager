e1afbfcd9aebca74316c84d5ca35b2a60d62744d
Adam <madfool.tiot@gmail.com>
2018-03-03 15:44:35 +1300

    Adding Monsters from Search

--- src/App.js
+++ src/App.js
@@ -9,45 +9,11 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    var creatures = new Map();
-    creatures.set(1, {
-        key: 1,
-        health: 7,
-      });
-    creatures.set(2, {
-        key: 2,
-        health: 7,
-      });
-    creatures.set(3, {
-        key: 3,
-        health: 8,
-      });
-    creatures.set(4, {
-        key: 4,
-        health: 8,
-      });
-    var monsters = new Map();
-    monsters.set('Goblin', {
-      key: 'Goblin',
-      defaultHealth: 7,
-      instances: [
-        1,
-        2,
-      ],
-    });
-    monsters.set('Orc', {
-      key: 'Orc',
-      defaultHealth: 8,
-      instances: [
-        3,
-        4,
-      ],
-    });
     this.state = {
-      monsterNames: mm.monster.map(monster => monster.name),
+      monsters: mm.monster,
       searchText: '',
-  		monsters: monsters,
-      creatures: creatures,
+  		monstersAdded: new Map(),
+      creatures: new Map(),
       nextCreatureKey: 5,
   	};
   }
@@ -58,12 +24,32 @@ class App extends Component {
 		});
   }
 
+  parseMonsterHealth = (monster) => {
+    var exp = new RegExp('([0-9]*).*');
+    return monster.hp.match(exp)[1];
+  }
+
+  addMonster = (index) => {
+    this.setState((previousState, currentProps) => {
+      var monstersAdded = new Map(previousState.monstersAdded);
+      monstersAdded.set(mm.monster[index].name, {
+        key: mm.monster[index].name,
+        defaultHealth: this.parseMonsterHealth(mm.monster[index]),
+        instances: [],
+      });
+			return {
+        monstersAdded: monstersAdded,
+        searchText: '',
+			};
+    });
+  }
+
   addCreature = (monster) => {
     this.setState((previousState, currentProps) => {
-      var monsters = new Map(previousState.monsters);
-      monsters.set(monster.key, Object.assign(previousState.monsters.get(monster.key), {
+      var monstersAdded = new Map(previousState.monstersAdded);
+      monstersAdded.set(monster.key, Object.assign(previousState.monstersAdded.get(monster.key), {
         instances: [
-          ...previousState.monsters.get(monster.key).instances,
+          ...previousState.monstersAdded.get(monster.key).instances,
           previousState.nextCreatureKey],
       }));
       var creatures = new Map(previousState.creatures);
@@ -72,7 +58,7 @@ class App extends Component {
         health: monster.defaultHealth,
       }));
 			return {
-        monsters: monsters,
+        monstersAdded: monstersAdded,
 				creatures: creatures,
         nextCreatureKey: previousState.nextCreatureKey + 1,
 			};
@@ -81,14 +67,14 @@ class App extends Component {
 
   removeCreature = (monster, key) => {
     this.setState((previousState, currentProps) => {
-      var monsters = new Map(previousState.monsters);
-      monsters.set(monster.key, Object.assign(previousState.monsters.get(monster.key), {
-        instances: previousState.monsters.get(monster.key).instances.filter(item => item !== key),
+      var monstersAdded = new Map(previousState.monstersAdded);
+      monstersAdded.set(monster.key, Object.assign(previousState.monstersAdded.get(monster.key), {
+        instances: previousState.monstersAdded.get(monster.key).instances.filter(item => item !== key),
       }));
       var creatures = new Map(previousState.creatures);
       creatures.delete(key);
 			return {
-        monsters: monsters,
+        monstersAdded: monstersAdded,
 				creatures: creatures,
 			};
     });
@@ -109,8 +95,8 @@ class App extends Component {
   render = () => {
     return (
       <div>
-        <MonsterSearch monsterNames={this.state.monsterNames} searchText={this.state.searchText} updateSearchText={this.updateSearchText} />
-        {Array.from(this.state.monsters).map(([key, monster]) => {
+        <MonsterSearch monsters={this.state.monsters} searchText={this.state.searchText} updateSearchText={this.updateSearchText} addMonster={this.addMonster} />
+        {Array.from(this.state.monstersAdded).map(([key, monster]) => {
           return <Monster key={key} creatures={this.state.creatures} addCreature={this.addCreature} removeCreature={this.removeCreature} setCreatureHealth={this.setCreatureHealth}>{monster}</Monster>
         })}
       </div>
--- src/MonsterSearch.js
+++ src/MonsterSearch.js
@@ -4,17 +4,23 @@ class MonsterSearch extends Component {
   render() {
     return (
       <div className="monster-search">
-        <p>Search</p>
+        <p>Search to add Monster</p>
         <input type="text" value={this.props.searchText} onChange={this.props.updateSearchText} />
-        {(this.props.searchText !== '') ?
-          this.props.monsterNames
-            .filter(monsterName =>
-              monsterName.toLowerCase().indexOf(this.props.searchText.toLowerCase()) !== -1)
-            .map((monsterName, index) => {
-              return <p key={index} >{monsterName}</p>
-            })
-          :
-          ''}
+        <ul className="monster-name-results">
+          {(this.props.searchText !== '')
+            ?
+            this.props.monsters
+              .map((monster, index) => {
+                if (monster.name.toLowerCase().indexOf(this.props.searchText.toLowerCase()) !== -1) {
+                  return <li className="monster-name" key={index} onClick={() => this.props.addMonster(index)} >{monster.name}</li>
+                }
+                else {
+                  return '';
+                }
+              })
+            :
+            ''}
+        </ul>
       </div>
     );
   }
