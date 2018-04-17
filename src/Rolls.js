1d62fa0875f742ec4c50addf9b748b41a4d8c6c7
Adam <madfool.tiot@gmail.com>
2018-04-17 14:07:27 +1200

    Highlighting crit success and crit fails

--- src/Rolls.js
+++ src/Rolls.js
@@ -8,11 +8,27 @@ class Rolls extends Component {
         return <div key={index} className="dice-roll">
           {roll.name !== undefined && <span className="roll-name">{roll.name}</span>}
           <span className="roll-total">{roll.total}</span>
-          <span className="roll-rolls">={roll.rolls.join('+')}</span>
+          {this.renderRolls(roll.rolls)}
           {roll.modifier !== undefined && <span className="roll-modifier">+{roll.modifier}</span>}
         </div>
       })}
     </div>;
+  }
+
+  renderRolls = rolls => {
+    return <span className="roll-rolls">
+      ={rolls.map(roll => {
+        return this.renderRoll(roll);
+      }).reduce((acc, x) => {
+        return acc === null ? [x] : [acc, <span>+</span>, x];
+      }, null)}
+    </span>
+  }
+
+  renderRoll = roll => {
+    return <span className={roll === 20 ? 'critical-success' : (roll === 1) ? 'critical-fail' : ''}>
+      {roll}
+    </span>
   }
 }
 
