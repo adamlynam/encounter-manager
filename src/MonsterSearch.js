38daf7b7ffd7dac61b4f3c8fb0e900515de0274b
Adam <madfool.tiot@gmail.com>
2018-04-26 23:27:45 +1200

    Adding CR and XP to search results

--- src/MonsterSearch.js
+++ src/MonsterSearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MonsterTools from './MonsterTools';
 
 class MonsterSearch extends Component {
   render() {
@@ -13,10 +14,19 @@ class MonsterSearch extends Component {
 
   renderSearchResults = () => {
     var resultIndex = -1;
-    return <ul className="monster-name-results">
+    return <ul className="monster-results">
+      {this.props.searchResults.size > 0 && <li className="heading">
+        <span className="monster-name">Monster</span>
+        <span className="monster-xp">XP</span>
+        <span className="monster-cr">CR</span>
+      </li>}
       {Array.from(this.props.searchResults).map(([key, monster]) => {
         resultIndex++;
-        return <li className={'monster-name' + ((resultIndex == this.props.searchSelected) ? ' selected' : '')} key={key} onClick={() => this.props.addMonster(key)} >{monster.name}</li>
+        return <li className={'monster-result' + ((resultIndex == this.props.searchSelected) ? ' selected' : '')} key={key} onClick={() => this.props.addMonster(key)} >
+          <span className="monster-name">{monster.name}</span>
+          <span className="monster-xp">{MonsterTools.parseMonsterXp(monster)}</span>
+          <span className="monster-cr">{MonsterTools.parseMonsterCr(monster)}</span>
+        </li>
       })}
       </ul>
   }
--- src/MonsterTools.js
+++ src/MonsterTools.js
@@ -1,6 +1,41 @@
 import monsterDescriptions from './data/descriptions/monster-descriptions.json';
 
 const longestNumberRegex = new RegExp('[0-9]+');
+const xpCrLookup = new Map();
+xpCrLookup.set('0', 10);
+xpCrLookup.set('1/8', 25);
+xpCrLookup.set('1/4', 50);
+xpCrLookup.set('1/2', 100);
+xpCrLookup.set('1', 200);
+xpCrLookup.set('2', 450);
+xpCrLookup.set('3', 700);
+xpCrLookup.set('4', 1100);
+xpCrLookup.set('5', 1800);
+xpCrLookup.set('6', 2300);
+xpCrLookup.set('7', 2900);
+xpCrLookup.set('8', 3900);
+xpCrLookup.set('9', 5000);
+xpCrLookup.set('10', 5900);
+xpCrLookup.set('11', 7200);
+xpCrLookup.set('12', 8400);
+xpCrLookup.set('13', 10000);
+xpCrLookup.set('14', 11500);
+xpCrLookup.set('15', 13000);
+xpCrLookup.set('16', 15000);
+xpCrLookup.set('17', 18000);
+xpCrLookup.set('18', 20000);
+xpCrLookup.set('19', 22000);
+xpCrLookup.set('20', 25000);
+xpCrLookup.set('21', 33000);
+xpCrLookup.set('22', 41000);
+xpCrLookup.set('23', 50000);
+xpCrLookup.set('24', 62000);
+xpCrLookup.set('25', 75000);
+xpCrLookup.set('26', 90000);
+xpCrLookup.set('27', 105000);
+xpCrLookup.set('28', 120000);
+xpCrLookup.set('29', 135000);
+xpCrLookup.set('30', 155000);
 
 function parseNumberFromListWithMatchingText(list, text) {
   if (list === undefined) {
@@ -23,6 +58,26 @@ function parseGroundSpeed(monster) {
 
 export function calculateModifier(value) {
   return Math.floor((value - 10) / 2);
+}
+
+export function parseMonsterCr(monster) {
+  if (typeof monster.cr === 'string' || monster.cr instanceof String) {
+    return monster.cr;
+  }
+  else {
+    return monster.cr.cr;
+  }
+}
+
+function calculateXPFromCR(cr) {
+  if (xpCrLookup.has(cr)) {
+    return xpCrLookup.get(cr);
+  }
+  return 0;
+}
+
+export function parseMonsterXp(monster) {
+  return calculateXPFromCR(parseMonsterCr(monster));
 }
 
 export function parseMonsterAc(monster) {
@@ -86,6 +141,8 @@ export function generateMonsterDescription(monster) {
 
 export default {
   calculateModifier,
+  parseMonsterCr,
+  parseMonsterXp,
   parseMonsterAc,
   parseMonsterSize,
   parseMonsterHealth,
