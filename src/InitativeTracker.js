bff6cd3013f3b8c6be911ddd898c2cbc3063d231
Adam <madfool.tiot@gmail.com>
2018-05-05 22:41:22 +1200

    Styling the initative tracker

--- src/InitativeTracker.js
+++ src/InitativeTracker.js
@@ -5,13 +5,18 @@ class InitativeTracker extends Component {
   render = () => {
     return <div className="initative-tracker">
       <h3>Initative</h3>
+      <ul className="list-group">
       {Array.from(this.props.monsters)
       .sort((a,b) => {
         return b[1].initative - a[1].initative;
       })
       .map(([key, monster]) => {
-        return <div key={key} className={'initative-row' + (key === this.props.currentInitative ? ' current' : '')}><span className="monster-name">{monster.name}</span><span className="monster-initative">{monster.initative}</span></div>
+        return <li key={key} className={'list-group-item initative-row' + (key === this.props.currentInitative ? ' current' : '')}>
+          <span className="monster-name">{monster.name}</span>
+          <span className="monster-initative">{monster.initative}</span>
+        </li>
       })}
+      </ul>
       <button type="button" className="btn btn-primary" onClick={() => this.props.advanceInitative()}>Next</button>
     </div>;
   }
